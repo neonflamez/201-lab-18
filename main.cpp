2cd1a4d1642646f6c646d3a00164e44ce9b37be6
Shayan Khan <neonclanflamez@gmail.com>
2024-10-06 21:39:56 -0800

    second commit

--- main.cpp
+++ main.cpp
@@ -1,1 +1,19 @@
 // COMSC 210 || Lab 18 || Shayan Khan
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Review{
+    float rating;
+    string comments;
+    Review* next;
+};
+
+void addHead(Review*& head, float rating, const string& comments){
+    Review* newReview = new Review{rating, comments, head};
+    head = newReview;
+}
+
+void addTail(Review*& head, float rating, const string& comments)
